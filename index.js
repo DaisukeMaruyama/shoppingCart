21d3998bcaf975911749b5c091d0b92c4db41fd3
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-08-31 11:59:16 +0900

    cartのカウント数を表示する

--- index.js
+++ index.js
@@ -1,11 +1,59 @@
 let carts = document.querySelectorAll(".add-cart");
 
+let products = [
+  {
+    name: "Product1",
+    tag: "product1",
+    price: 15, 
+    inCart: 0
+  },
+  {
+  name: "Product2",
+  tag: "product2",
+  price: 20, 
+  inCart: 0
+  },
+  {
+  name: "Product3",
+  tag: "product3",
+  price: 27, 
+  inCart: 0
+  },
+  {
+  name: "Product4",
+  tag: "product4",
+  price: 15, 
+  inCart: 0
+  }
+];
+
 for (let i = 0; i < carts.length; i++) {
   carts[i].addEventListener("click", () => {
-    cartNumbers()
+    cartNumbers();
   })
 }
 
-function cartNumbers() {
-  localStorage.setItem("cartNumbers", 1);
+function onLordCartNumbers() {
+  let productNumbers = localStorage.getItem("cartNumbers");
+
+  if (productNumbers) {
+    document.querySelector(".cart span").textContent = productNumbers;
+  }
 }
+
+function cartNumbers() {
+  let productNumbers = localStorage.getItem("cartNumbers");
+
+  productNumbers = parseInt(productNumbers);
+
+  if (productNumbers) {
+    localStorage.setItem("cartNumbers", productNumbers + 1);
+    document.querySelector(".cart span").textContent = productNumbers + 1;
+  } else {
+    localStorage.setItem("cartNumbers", 1);
+    document.querySelector(".cart span").textContent = 1;
+  }
+
+}
+
+onLordCartNumbers();
